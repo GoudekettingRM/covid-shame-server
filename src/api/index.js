2049d06e2da37888b7a804a919b9038db5b4764d
GoudekettingRM <r.goudeketting@gmail.com>
2020-08-25 22:33:05 +0200

    Move rate limit and slowdown to api folder

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
+const router = express.Router();
+
+const mainRateLimiter = require('./utils/rateLimitConfig');
+const mainSpeedLimiter = require('./utils/slowDownConfig');
+
+router.use(mainRateLimiter);
+router.use(mainSpeedLimiter);
+
 const imagesRouter = require('./image/router');
 const userRouter = require('./user/router');
 const authRouter = require('./auth/authRouter');
 const likeRouter = require('./likes/router');
-
-const router = express.Router();
 
 router.use('/images', imagesRouter);
 router.use('/users', userRouter);
